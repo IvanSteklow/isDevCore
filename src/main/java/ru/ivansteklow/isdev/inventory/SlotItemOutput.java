508bf1c5bc17e32d4400356e7ecf5c032b11295f
IvanSteklow <ivansteklow@gmail.com>
2018-05-12 15:08:28 +0300

    Output slot added

---
+++ src/main/java/ru/ivansteklow/isdev/inventory/SlotItemOutput.java
@@ -0,0 +1,18 @@
+package ru.ivansteklow.isdev.inventory;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+
+public class SlotItemOutput extends SlotItemHandler {
+
+	public SlotItemOutput(IItemHandler itemHandler, int index, int xPosition, int yPosition) {
+		super(itemHandler, index, xPosition, yPosition);
+	}
+	
+	@Override
+	public boolean isItemValid(ItemStack stack) {
+		return false;
+	}
+
+}
